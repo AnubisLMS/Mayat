ba61653829d14fc24965debd3092e2cf8f85c847
Maxwell Yang <gabrielyang233@outlook.com>
2022-03-26 21:28:14 -0400

    CHG Move C related code out to C.py

--- anubis_pd.py
+++ anubis_pd.py
@@ -1,9 +1,8 @@
 from hashlib import sha256
-import clang.cindex
 import os
+import sys
 import argparse
 from datetime import datetime
-import sys
 import textwrap
 
 # Base class for AST classes for other programming languages
@@ -43,29 +42,8 @@ class AST:
         return lst
     
     @classmethod
-    def create(cls, node, parent=None):
+    def create(cls, node):
         NotImplemented
-
-class C_AST(AST):
-    def __init__(self, parent=None, name=None, pos=None, kind=None):
-        AST.__init__(self, parent, name, pos, kind)
-    
-    @classmethod
-    def create(cls, node, parent=None):
-        c_ast_node = C_AST(
-            parent=parent,
-            name=node.spelling,
-            pos=(node.location.line, node.location.column),
-            kind=node.kind
-        )
-
-        c_ast_node.weight = 1
-        for child in node.get_children():
-            child_node = C_AST.create(child, c_ast_node)
-            c_ast_node.children.append(child_node)
-            c_ast_node.weight += child_node.weight
-
-        return c_ast_node
 
 class Checker:
     def __init__(self, path1, path2, arrLL1, arrLL2, threshold=5):
@@ -110,62 +88,47 @@ class Checker:
 
         self.similarity = num_of_same_nodes / len(arrLL)
 
-def main():
-    parser = argparse.ArgumentParser(
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-        description="Anubis AntiCheat",
-        epilog=textwrap.dedent('''
-        Explain:
-            The script will find all code under the path:
-                <DIR>/<any dirname>/<SUBPATH>
+arg_parser = argparse.ArgumentParser(
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    description="Anubis AntiCheat",
+    epilog=textwrap.dedent('''
+    Explain:
+        The script will find all code under the path:
+            <DIR>/<any dirname>/<SUBPATH>
 
-            For example, `python3 anubis_pd.py -d /home/homework -p dir1/prog.c` will match:
-                /home/homework/<any dirname>/dir1/prog.c
-        ''')
-    )
-    parser.add_argument(
-        '-d',
-        dest="dir",
-        help="The main directory storing the code",
-        required=True,
-    )
-    parser.add_argument(
-        '-p',
-        dest="subpath",
-        help="The path relative to the directories under the main directory to the code itself",
-        required=True,
-    )
-    parser.add_argument(
-        '--threshold',
-        type=int,
-        default=5,
-        help="The threshold value controlling the granularity of the matching. Default 5",
-    )
-    parser.add_argument(
-        '--libclang',
-        dest="libclang_path",
-        help="The path to libclang, a C API used for analyzing the AST of C code"
-    )
+        For example, `python3 anubis_pd.py -d /home/homework -p dir1/prog.c` will match:
+            /home/homework/<any dirname>/dir1/prog.c
+    ''')
+)
+arg_parser.add_argument(
+    '-d',
+    dest="dir",
+    help="The main directory storing the code",
+    required=True,
+)
+arg_parser.add_argument(
+    '-p',
+    dest="subpath",
+    help="The path relative to the directories under the main directory to the code itself",
+    required=True,
+)
+arg_parser.add_argument(
+    '--threshold',
+    type=int,
+    default=5,
+    help="The threshold value controlling the granularity of the matching. Default 5",
+)
 
-    args = parser.parse_args()
-
-    # /Library/Developer/CommandLineTools/usr/lib
-    if args.libclang_path is not None:
-        clang.cindex.Config.set_library_path(args.libclang_path)
-        index = clang.cindex.Index.create()
-    else:
-        try:
-            index = clang.cindex.Index.create()
-        except clang.cindex.LibclangError:
-            print("Cannot find libclang. Please specify its path using --libclang argument")
-            sys.exit()
-
+def driver(AST_class, args):
+    # Print current time and the raw command
     print(datetime.now())
     print(' '.join(sys.argv))
     print()
 
+    # Start datetime
     start_time = datetime.now()
 
+    # Translate all code to ASTs
     asts = {}
     for dirname in os.listdir(args.dir):
         path = f"{args.dir}/{dirname}/{args.subpath}"
@@ -173,11 +136,11 @@ def main():
             print(f"{args.dir}/{dirname} doesn't have {args.subpath}")
             continue
 
-        prog = index.parse(path)
-        c_ast = C_AST.create(prog.cursor)
-        c_ast.hash()
-        asts[path] = c_ast
+        ast = AST_class.create(path)
+        ast.hash()
+        asts[path] = ast
 
+    # Run matching algorithm
     keys = list(asts.keys())
     results = []
     for i in range(len(keys)):
@@ -193,18 +156,17 @@ def main():
             )
 
             checker.check()
-
             results.append(checker)
 
+    # Stop datetime
     end_time = datetime.now()
 
+    # Print total time used
     print()
     print(f"Duration: {(end_time-start_time).seconds}s")
     print()
 
+    # Print similarities in descending order
     results.sort(key=lambda x: -x.similarity)
     for r in results:
         print(f"{r.path1} - {r.path2}:\t{r.similarity:%}")
-
-if (__name__ == "__main__"):
-    main()
