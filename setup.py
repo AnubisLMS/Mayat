dd20030215be55e9c51322f4afcafc08112b61ed
Tian(Maxwell) Yang <maxwell_yang@outlook.com>
2023-02-20 22:05:13 -0500

    CHG LICENSE, MANIFEST.in, and setup.py

--- setup.py
+++ setup.py
@@ -16,31 +16,14 @@ with open("requirements_dev.txt") as requirements_dev_file:
     test_requirements = parse_requirements(requirements_dev_file.read())
 
 setup(
+    name="Mayat",
+    version="1.0.0",
     author="Tian(Maxwell) Yang",
-    python_requires=">=3.5",
-    classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Intended Audience :: Developers",
-        "Natural Language :: English",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-    ],
-    description="Anubis Plagerism Detection",
-    entry_points={
-        "console_scripts": [
-            "anubis=anubis.cli:main",
-        ],
-    },
-    install_requires=requirements,
-    include_package_data=True,
-    keywords="anubis",
-    name="anubis-pd",
-    packages=find_packages(include=["anubis_pd", "anubis_pd.*"]),
-    # setup_requires=setup_requirements,
-    test_suite="tests",
-    tests_require=test_requirements,
-    version="0.0.0",
-    zip_safe=False,
+    author_email="ty1146@nyu.edu",
+    packages=find_packages(include=["mayat", "mayat.*"]),
+    url="https://github.com/AnubisLMS/Mayat",
+    license="LICENSE.txt",
+    description="AST-based code similarity detection tool",
+    long_description=open('README.md').read(),
+    install_requires=requirements
 )
