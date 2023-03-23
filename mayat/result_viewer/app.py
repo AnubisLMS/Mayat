b47d52b865a298dac654cb593eef18026c553a80
Tian(Maxwell) Yang <maxwell_yang@outlook.com>
2023-03-23 18:51:43 -0400

    ADD Code Viewer

--- mayat/result_viewer/app.py
+++ mayat/result_viewer/app.py
@@ -11,11 +11,23 @@ data = None
 def main():
     return render_template("main.html", data=data)
 
-@app.route("/<submission_A>/<submission_B>")
-def result(submission_A, submission_B):
-    submission_A = base64.urlsafe_b64decode(submission_A).decode()
-    submission_B = base64.urlsafe_b64decode(submission_B).decode()
-    return render_template("result.html", submission_A=submission_A, submission_B=submission_B)
+@app.route("/<int:index>")
+def result(index):
+    entry = data["result"][index - 1]
+
+    with open(entry["submission_A"], 'r') as f:
+        submission_A_code = f.read()
+    with open(entry["submission_B"], 'r') as f:
+        submission_B_code = f.read()
+
+    return render_template(
+        "result.html",
+        entry=entry,
+        submission_A_code=submission_A_code,
+        submission_B_code=submission_B_code,
+        prev=f"/{index - 1 if index > 1 else index}",
+        next=f"/{index + 1 if index < len(data['result']) else index}"
+    )
 
 if __name__ == "__main__":
     data = json.loads(open(sys.argv[1]).read())
