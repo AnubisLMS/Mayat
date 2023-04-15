bf3fdb00969d71b132fab51b57ba717eb3c0e157
Tian(Maxwell) Yang <maxwell_yang@outlook.com>
2023-04-15 15:07:46 -0400

    CHG Add flask to requirements

--- mayat/benchmark/mayatRunner.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ulimit -s hard
-
-root=`dirname $1`
-dir=`basename $1`
-paths=$root/$dir/*/*
-
-python3 -m mayat.frontends.Java $paths -o JSON | python3 -m mayat.benchmark.json_to_csv
